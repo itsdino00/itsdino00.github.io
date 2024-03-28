80387120879860c057ad7fbb86cfdeeb662b894f
itsdino00 <165156971+itsdino00@users.noreply.github.com>
2024-03-28 16:41:42 +0530

    Update

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>SOLANA TOOLS</h1>
+      </header>
+      <main>
+        {/* Content */}
+      </main>
+      <footer>
+        {/* Footer content */}
+      </footer>
+    </div>
+  );
+}
+
+export default App;
