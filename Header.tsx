b52694c464b5e49cf6cda04a1d50eadec90e6a29
itsdino00 <165156971+itsdino00@users.noreply.github.com>
2024-03-28 16:27:45 +0530

    Update

---
+++ Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header style={{ backgroundColor: '#333', color: 'cream' }}>
+      <h1>Solana Tools</h1>
+    </header>
+  );
+}
+
+export default Header;
